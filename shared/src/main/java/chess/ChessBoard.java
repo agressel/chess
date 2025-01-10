86bc44a8b598eb0ffb4329ed7cc93fa2d9bc8d24
agressel <gresselalan@gmail.com>
2025-01-09 19:50:43 -0700

    first commit

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,9 +7,11 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private ChessPiece[][] board;
 
     public ChessBoard() {
-        
+        this.board = new ChessPiece[8][8];
+        resetBoard();
     }
 
     /**
@@ -19,9 +21,15 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
-    }
+        if (position.getRow() < 1 || position.getRow() > 8 || position.getColumn() < 1 || position.getColumn() > 8) {
+            throw new IllegalArgumentException("Position is out of bounds: " + position);
+        }
 
+        int row = position.getRow() - 1;
+        int col = position.getColumn() - 1;
+
+        board[row][col]=piece;
+    }
     /**
      * Gets a chess piece on the chessboard
      *
@@ -30,14 +38,49 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
-    }
+        if (position.getRow() < 1 || position.getRow() > 8 || position.getColumn() < 1 || position.getColumn() > 8) {
+            throw new IllegalArgumentException("Position is out of bounds: " + position);
+        }
 
+    int row = position.getRow() - 1;
+    int col = position.getColumn() - 1;
+
+    return board[row][col];
+}
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
-    }
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                board[row][col] = null;
+            }
+        }
+        for (int col = 0; col < 8; col++) {
+            board[1][col] = new ChessPiece(PieceType.PAWN, "WHITE");
+        }
+        for (int col = 0; col < 8; col++) {
+            board[6][col] = new ChessPiece(PieceType.PAWN, "BLACK");
+        }
+        board[0][0] = new ChessPiece(PieceType.ROOK, "WHITE");
+        board[0][1] = new ChessPiece(PieceType.KNIGHT, "WHITE");
+        board[0][2] = new ChessPiece(PieceType.BISHOP, "WHITE");
+        board[0][3] = new ChessPiece(PieceType.QUEEN, "WHITE");
+        board[0][4] = new ChessPiece(PieceType.KING, "WHITE");
+        board[0][5] = new ChessPiece(PieceType.BISHOP, "WHITE");
+        board[0][6] = new ChessPiece(PieceType.KNIGHT, "WHITE");
+        board[0][7] = new ChessPiece(PieceType.ROOK, "WHITE");
+
+        // Step 5: Place black major pieces
+        board[7][0] = new ChessPiece(PieceType.ROOK, "BLACK");
+        board[7][1] = new ChessPiece(PieceType.KNIGHT, "BLACK");
+        board[7][2] = new ChessPiece(PieceType.BISHOP, "BLACK");
+        board[7][3] = new ChessPiece(PieceType.QUEEN, "BLACK");
+        board[7][4] = new ChessPiece(PieceType.KING, "BLACK");
+        board[7][5] = new ChessPiece(PieceType.BISHOP, "BLACK");
+        board[7][6] = new ChessPiece(PieceType.KNIGHT, "BLACK");
+        board[7][7] = new ChessPiece(PieceType.ROOK, "BLACK");
+
 }
+
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -10,12 +10,18 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ChessGame.TeamColor pieceColor;
+    private ChessPiece.PieceType pieceType;
+
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
     }
 
     /**
      * The various different chess piece options
      */
+
     public enum PieceType {
         KING,
         QUEEN,
@@ -28,15 +34,18 @@ public class ChessPiece {
     /**
      * @return Which team this chess piece belongs to
      */
+
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
+
     }
 
     /**
      * @return which type of chess piece this piece is
      */
+
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return pieceType;
     }
 
     /**
@@ -47,6 +56,6 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        //before this, do position mvoe and game
     }
 }
